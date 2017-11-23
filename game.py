14328b4e11a6333fe12911ad65c1bfdb969a2610
yoann-dufresne <yoann.dufresne0@gmail.com>
2017-11-23 18:38:56 +0100

    first commit

---
+++ game.py
@@ -0,0 +1,6 @@
+
+class Game:
+  def __init__(self, nb_scene):
+    self.nb_scene = nb_scene
+    self.scenes = [False for _ in range(nb_scene)]
+
---
+++ main.py
@@ -0,0 +1,8 @@
+from game import Game
+
+def main ():
+	game = Game(3)
+	print (game.scenes)
+
+if __name__ == '__main__':
+	main()

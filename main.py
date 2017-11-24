10d72f47c4c034a0b0d85555b63cd7064012ec2e
yoann-dufresne <yoann.dufresne0@gmail.com>
2017-11-24 21:05:07 +0100

    base game functions

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from game import Game
 
 def main ():
-	game = Game(3)
-	print (game.scenes)
+  game = Game(3)
+  print (game.scenes)
+  print (game.collide())
+  game.scenes[0][0] = True
+  print (game.scenes)
+  print (game.collide())
+  game.player_relative_move(65)
+  print (game.player_position)
+  game.player_absolute_move(355)
+  print (game.player_position)
+  game.create_walls(0.7)
+  print(game.scenes)
+
 
 if __name__ == '__main__':
-	main()
+  main()

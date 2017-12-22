d2f277365a7cd4e239342bbf494d3c4dbf58ae0d
yoann-dufresne <yoann.dufresne0@gmail.com>
2017-12-22 16:52:32 +0100

    Start of the serv/client mode

--- client.py
+++ client.py
@@ -1,18 +1,59 @@
 import zmq
+import time
+from threading import Thread
 
-context = zmq.Context()
 
-#  Socket to talk to server
-print("Connecting to hello world server…")
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
 
-#  Do 10 requests, waiting each time for a response
-for request in range(10):
-    print("Sending request %s …" % request)
-    socket.send(b"Hello")
+class Client:
+	def __init__ (self, ip='127.0.0.1'):
+		self.context = zmq.Context()
+		self.socket = self.context.socket(zmq.REQ)
+		self.serv_addr = "tcp://{}:5555".format(ip)
+		self.socket.connect(self.serv_addr)
 
-    #  Get the reply.
-    message = socket.recv()
-    print("Received reply %s [ %s ]" % (request, message))
+		self.socket.send(b"connect")
+		message = int(self.socket.recv().decode('utf-8'))
+		self.is_connected = True
+		self.idx = message
+
+
+	def start (self):
+		self.ping_thread = Thread(target=self.ping)
+		self.ping_thread.start()
+
+
+	def ping (self):
+		# Send ping request each 5 seconds
+		while self.is_connected:
+			time.sleep(5)
+			# Send ping request
+			self.socket.send('ping {}'.format(self.idx).encode('utf-8'))
+			# Wait answer for half a second
+			time.sleep(0.5)
+			try:
+				# Read the server answer
+				msg = self.socket.recv(zmq.NOBLOCK).decode('utf-8')
+				if msg != 'pong':
+					print("Server answer \"{}\" instead of pong".format(msg))
+					self.is_connected = False
+			except zmq.ZMQError:
+				print("Server is not answering to ping request")
+				self.is_connected = False
+	
+
+	def stop (self):
+		self.is_connected = False
+		self.ping_thread.join()
+		print("joined")
+		self.context.destroy()
+		print("destroyed")
+
+
+if __name__ == "__main__":
+	client = Client()
+	client.start()
+
+	while client.is_connected:
+		time.sleep(1)
+	client.stop()
     
--- server.py
+++ server.py
@@ -1,18 +1,64 @@
 import time
 import zmq
+from threading import Thread
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
 
-while True:
-    #  Wait for next request from client
-    message = socket.recv()
-    print("Received request: %s" % message)
+class Server:
+	def __init__ (self):
+		# Serrver configuration
+		self.context = zmq.Context()
+		self.socket = self.context.socket(zmq.REP)
+		self.socket.bind("tcp://*:5555")
+		# Loop init
+		self.is_alive = True
 
-    #  Do some 'work'
-    time.sleep(1)
+		# Create empty dictonnary for clients
+		self.clients = {}
+		self.next_id = 0
 
-    #  Send reply back to client
-    socket.send(b"World")
-    
+	def start (self):
+		# Start listening for clients in a thread
+		self.process = Thread(target=self.register_client)
+		self.process.start()
+
+	def register_client(self):
+		while self.is_alive:
+			try:
+				#  Wait for next request from client
+				message = self.socket.recv(zmq.NOBLOCK)
+				message = message.decode('utf-8')
+
+				client_id = -1
+				if message == "connect":
+					# Send id to the client
+					self.socket.send("{}".format(self.next_id).encode('utf-8'))
+					client_id = self.next_id
+					# Update the client next id
+					self.next_id += 1
+				elif message.startswith("ping"):
+					print(message)
+					client_id = int(message.strip().split(' ')[1])
+					self.socket.send(b"pong")
+				else:
+					print('Unrecognized message: {}'.format(message))
+					self.socket.send(b'unrecognized message')
+
+				# Save last connection date of the client
+				if client_id != -1:
+					self.clients[client_id] = time.time()
+			except zmq.ZMQError:
+				time.sleep(0.1)
+
+	def stop (self):
+		self.is_alive = False
+		self.context.destroy()
+		self.process.join()
+		print ("Exited")
+
+
+serv = Server()
+serv.start()
+
+time.sleep(10)
+serv.stop()
+

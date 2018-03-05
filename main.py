75095b47f55c1d5ada3a17c7d9a9597ddaf392da
Yoann Dufresne <yoann.dufresne0@gmail.com>
2018-03-05 10:41:32 +0000

    pep8

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from game import Game
 
-def main ():
-  game = Game(3)
-  print (game.scenes)
-  print (game.collide())
-  game.scenes[0][0] = True
-  print (game.scenes)
-  print (game.collide())
-  game.player_relative_move(65)
-  print (game.player_position)
-  game.player_absolute_move(355)
-  print (game.player_position)
-  game.create_walls(0.7)
-  print(game.scenes)
+
+def main():
+    game = Game(3)
+    print (game.scenes)
+    print (game.collide())
+    game.scenes[0][0] = True
+    print (game.scenes)
+    print (game.collide())
+    game.player_relative_move(65)
+    print (game.player_position)
+    game.player_absolute_move(355)
+    print (game.player_position)
+    game.create_walls(0.7)
+    print(game.scenes)
 
 
 if __name__ == '__main__':
-  main()
+    main()
--- server.py
+++ server.py
@@ -4,59 +4,60 @@ from threading import Thread
 
 
 class Server:
-	def __init__ (self):
-		# Serrver configuration
-		self.context = zmq.Context()
-		self.socket = self.context.socket(zmq.REP)
-		self.socket.bind("tcp://*:5555")
-		# Loop init
-		self.is_alive = True
+    def __init__(self):
+        # Server configuration
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.REP)
+        self.socket.bind("tcp://*:5555")
+        # Loop init
+        self.is_alive = True
 
-		# Create empty dictonnary for clients
-		self.clients = {}
-		self.next_id = 0
+        # Create empty dictonnary for clients
+        self.clients = {}
+        self.next_id = 0
+        self.process = None
 
-	def start (self):
-		# Start listening for clients in a thread
-		self.process = Thread(target=self.register_client)
-		self.process.start()
+    def start(self):
+        # Start listening for clients in a thread
+        self.process = Thread(target=self.register_client)
+        self.process.start()
 
-	def register_client(self):
-		while self.is_alive:
-			try:
-				#  Wait for next request from client
-				message = self.socket.recv(zmq.NOBLOCK)
-				message = message.decode('utf-8')
+    def register_client(self):
+        while self.is_alive:
+            try:
+                #  Wait for next request from client
+                message = self.socket.recv(zmq.NOBLOCK)
+                message = message.decode('utf-8')
 
-				client_id = -1
-				if message == "connect":
-					# Send id to the client
-					self.socket.send("{}".format(self.next_id).encode('utf-8'))
-					client_id = self.next_id
-					# Update the client next id
-					self.next_id += 1
-				elif message.startswith("ping"):
-					print(message)
-					client_id = int(message.strip().split(' ')[1])
-					self.socket.send(b"pong")
-				elif message.startswith("latency"):
-					print(message)
-					self.socket.send("{}".format(time.time()).encode('utf-8'))
-				else:
-					print('Unrecognized message: {}'.format(message))
-					self.socket.send(b'unrecognized message')
+                client_id = -1
+                if message == "connect":
+                    # Send id to the client
+                    self.socket.send("{}".format(self.next_id).encode('utf-8'))
+                    client_id = self.next_id
+                    # Update the client next id
+                    self.next_id += 1
+                elif message.startswith("ping"):
+                    print(message)
+                    client_id = int(message.strip().split(' ')[1])
+                    self.socket.send(b"pong")
+                elif message.startswith("latency"):
+                    print(message)
+                    self.socket.send("{}".format(time.time()).encode('utf-8'))
+                else:
+                    print('Unrecognized message: {}'.format(message))
+                    self.socket.send(b'unrecognized message')
 
-				# Save last connection date of the client
-				if client_id != -1:
-					self.clients[client_id] = time.time()
-			except zmq.ZMQError:
-				time.sleep(0.1)
+                # Save last connection date of the client
+                if client_id != -1:
+                    self.clients[client_id] = time.time()
+            except zmq.ZMQError:
+                time.sleep(0.1)
 
-	def stop (self):
-		self.is_alive = False
-		self.context.destroy()
-		self.process.join()
-		print ("Exited")
+    def stop(self):
+        self.is_alive = False
+        self.context.destroy()
+        self.process.join()
+        print ("Exited")
 
 
 serv = Server()

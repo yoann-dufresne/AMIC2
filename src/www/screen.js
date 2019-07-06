dbe9ddc0ecf2d599df5dc71daf523fd9e2bb0af6
Yoann Dufresne <yoann.dufresne0@gmail.com>
2019-07-06 15:30:59 +0200

    add computation exceptions for 1 screen game

--- src/www/screen.js
+++ src/www/screen.js
@@ -1,16 +1,27 @@
 
 class Screen {
-	constructor(game=new Game()) {
-		this.canvas = document.getElementById("screen");
+	constructor(position, canvas=undefined) {
+		if (canvas == undefined)
+			this.canvas = document.getElementById("screen");
+		else
+			this.canvas = canvas;
+
+		// TODO: Change to adapt on the wanted size
 		this.canvas.width = window.innerWidth;
 		this.canvas.height = window.innerHeight;
+		// /TODO
 		this.ctx = this.canvas.getContext('2d');
 
 		this.screen_idx = 0
 		this.nb_screen = 1
 
-		this.game = game;
+		this.position = position;
 		this.start_refresh();
+	}
+
+	resize(width, height) {
+		this.canvas.width = width;
+		this.canvas.height = height;
 	}
 
 	update_screen_context(screen_idx, nb_screens) {
@@ -32,13 +43,17 @@ class Screen {
 		this.ctx.fillStyle = "black";
 		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 		
-		let absolute_position = this.game.player*this.nb_screens;
+		let absolute_position = this.position.player*this.nb_screens;
 		let relative_position = absolute_position - this.screen_idx;
 		// Exceptions for extremities
 		if (this.screen_idx == this.nb_screens - 1 && absolute_position < 0.5) {
-			relative_position += this.nb_screens;
+			let tmp = relative_position + this.nb_screens;
+			if (tmp-0.5 < 0.5-relative_position)
+				relative_position = tmp;
 		} else if (this.screen_idx == 0 && absolute_position > this.nb_screens - 0.5) {
-			relative_position -= this.nb_screens;
+			let tmp = relative_position - this.nb_screens;
+			if (0.5-tmp < relative_position - 0.5)
+				relative_position = tmp;
 		}
 
 		// Draw player
